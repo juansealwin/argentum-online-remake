c2c0da4abdfa1899fbffe221853557437b525bf2
pintotomas <tomasp834@gmail.com>
2020-06-17 11:27:39 -0300

    fix

--- server/base_character.h
+++ server/base_character.h
@@ -5,13 +5,15 @@
 
 class BaseCharacter : public Entity {
  public:
-+  //Este primer constructor lo usa la clase hija Monster
-+  BaseCharacter(int x, int y, int type, char repr, int max_hp, int level, Map *map);
-+  //Este segundo constructor lo usa el heroe ya que max_hp en ese caso es calculado
-+  BaseCharacter(int x, int y, int type, char repr, int level, Map *map);
-+  //Este tercer constructor es temporal para los npcs (sacerdote, etc) ya que no tienen nada que ver con los monstruos/heroes
-+  //se puede borrar esto y hacer que esos npcs hereden directamente de entidad
-+  BaseCharacter(int x, int y, int type, char repr, Map *map);
+  // Este primer constructor lo usa la clase hija Monster
+  BaseCharacter(int x, int y, int type, char repr, int max_hp, int level);
+  // Este segundo constructor lo usa el heroe ya que max_hp en ese caso es
+  // calculado
+  BaseCharacter(int x, int y, int type, char repr, int level);
+  // Este tercer constructor es temporal para los npcs (sacerdote, etc) ya que
+  // no tienen nada que ver con los monstruos/heroes se puede borrar esto y hacer
+  // que esos npcs hereden directamente de entidad
+  BaseCharacter(int x, int y, int type, char repr);
   virtual ~BaseCharacter();
   BaseCharacter(const BaseCharacter &) = delete;
 
@@ -21,7 +23,6 @@ class BaseCharacter : public Entity {
   int current_hp;
   int type;
   char representation;
-
 };
 
 #endif  // BASE_CHARACTER_H
