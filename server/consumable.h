3e1788146af4f834bff9f333b0978e2e0c2c970b
pintotomas <tomasp834@gmail.com>
2020-06-18 11:20:04 -0300

    fix?

--- server/consumable.h
+++ server/consumable.h
@@ -4,10 +4,10 @@
 class Consumable : Item {
  public:
   Consumable(int id, unsigned int mana_regen, unsigned int hp_regen);
-  virtual bool can_be_used_by(Hero &hero);
   virtual ~Consumable();
-  Consumable(const Consumable &) = delete;
   virtual void self_apply(Hero &hero);
-  unsigned int mana_regen, hp_regen, range;
+  Consumable(const Consumable &) = delete;
+
+  unsigned int mana_regen, hp_regen;
 };
 #endif  // CONSUMABLE_H
