9ac6eb5fd610f22e1e4261bbff0aa39bf083b210
rodrigosettle <rodrigo@settlenetwork.com>
2020-06-23 13:33:26 -0300

    paso al server los comandos

--- server/move_command.cpp
+++ server/move_command.cpp
@@ -2,12 +2,13 @@
 
 #include <iostream>
 
+#include "argentum_game.h"
+
 MoveCommand::MoveCommand(const int entity_id, const int next_x, const int next_y)
     : entity_id(entity_id), next_x(next_x), next_y(next_y) {}
 
 MoveCommand::~MoveCommand() {}
 
 void MoveCommand::execute(ArgentumGame *game) {
-  // ESTO SI VA. lo comenté porque tira invalid use of incomplete type ‘class ArgentumGame’
-  // game->move_entity(entity_id, next_x, next_y);
+  game->move_entity(entity_id, next_x, next_y);
 }
