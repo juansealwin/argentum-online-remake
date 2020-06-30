eb802f62d3861a47c2d30c0d03a4c230fc4796db
juansealwin <juan.alvarezwindey@gmail.com>
2020-06-30 20:10:06 -0300

    cambio en render

--- client/playable_character.cpp
+++ client/playable_character.cpp
@@ -88,20 +88,20 @@ void PlayableCharacter::render(SDL_Renderer* renderer, int x_rel, int y_rel) {
   }
   // Renderizamos cabeza
   texture_manager.get_texture(type_head).render(
-      renderer, &head_rect, x - x_rel,
+      renderer, &head_rect, x + head_rect.w / 4 - x_rel,
       y - height / 2 - head_rect.h / 2 - y_rel);
 
   // Si tiene el casco equipado lo renderizamos
   if (helmet) {
     texture_manager.get_texture(helmet)
-        .render(renderer, &head_rect, x - x_rel,
+        .render(renderer, &head_rect, x + head_rect.w / 4 - x_rel,
                 y - height / 2 - head_rect.h / 2 - y_rel);
   }
 
   // Si tiene el arma equipada la renderizamos
   if (weapon) {
     texture_manager.get_texture(weapon)
-        .render(renderer, &body_rect, x - x_rel,
+        .render(renderer, &body_rect, x - width / 2 - x_rel,
                 y - height / 2 - y_rel);
   }
 }
