b2718d391bdc0f0c5f35217b94a4b5d177dc99aa
pintotomas <tomasp834@gmail.com>
2020-07-13 01:20:17 -0300

    fix crash server

--- server/message_center.cpp
+++ server/message_center.cpp
@@ -6,28 +6,24 @@ MessageCenter::~MessageCenter() {}
 
 void MessageCenter::add_player(std::string player_name, BlockingThreadSafeQueue<Notification *> *notification_queue) {
   std::unique_lock<std::mutex> lock(mutex);
-  //std::cout << "added player with name: " << player_name << std::endl;
   players_notification_queues.emplace(player_name, notification_queue);
 }
 void MessageCenter::remove_player(std::string player_name) {
   std::unique_lock<std::mutex> lock(mutex);
-  //std::cout << "removing player with name: " << player_name << std::endl;
-  if (players_notification_queues.count(player_name) > 0) {
-    players_notification_queues.erase(player_name);
-  }
+  if (players_notification_queues.count(player_name) < 0) return;
+  players_notification_queues.erase(player_name);
+  
 }
   //Agrega el nombre del origen al mensaje y crea una notificacion de mensaje a ser enviada a player_dst
 void MessageCenter::send_message(std::string dst, std::string message) {
+  if(players_notification_queues.count(dst) < 0) return;
   BlockingThreadSafeQueue<Notification *> *q = players_notification_queues.at(dst);
   if(!q) return;
   std::vector<unsigned char> notification;
-  // std::vector<unsigned char>::iterator it;
-  // mover a la clase
   uint8_t notification_id = 4;
   uint8_t message_length = message.length();
   notification.push_back(notification_id);
   notification.push_back(message_length);
-  // it = notification.end();
   notification.insert(notification.end(), message.begin(), message.end());
   MessageNotification *message_notification = new MessageNotification(notification);
   q->push(message_notification);
@@ -39,17 +35,12 @@ void MessageCenter::notify_damage_received(std::string attacked, unsigned int dm
   if (dmg == 0) {
    message = "Blocked attack from " + attacker + "!";
   }
-  //BlockingThreadSafeQueue<Notification *> *q = players_notification_queues.at(attacked);
   send_message(attacked, message);
-  // if(q) {
-  //std::cout << "Sending message: " << message << std::endl;
-  // }
 }
 
 void MessageCenter::send_private_message(std::string src, std::string dst, std::string message) {
   std::unique_lock<std::mutex> lock(mutex);
-  std::string msg = dst + ": " + message;
-  //std::cout << "destination is " << dst << std::endl;
+  std::string msg = src + ": " + message;
   send_message(dst, msg);
 }
 
@@ -67,14 +58,7 @@ void MessageCenter::send_not_enough_gold_message(std::string dst, unsigned int p
 void MessageCenter::notify_damage_done(std::string attacker, unsigned int dmg, std::string attacked) {
   std::unique_lock<std::mutex> lock(mutex);
   std::string message = "Damage done: " + std::to_string(dmg) + " to " + attacked;
-  if (dmg == 0) {
-   message = attacker + " blocked your attack!";
-  }
+  if (dmg == 0) message = attacker + " blocked your attack!";
   send_message(attacker, message);
-
-  //BlockingThreadSafeQueue<Notification *> *q = players_notification_queues.at(attacker);
-  // if(q) {
-  //   std::cout << "Sending message: " << message << std::endl;
-  // }
 
 }
