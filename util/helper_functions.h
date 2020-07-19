52d109bbb2f018ca9cac4e4ba799655ff5086691
pintotomas <tomasp834@gmail.com>
2020-07-19 15:47:11 -0300

    se mueven algunas constantes mas a la cfg, arreglo envio de enies

--- util/helper_functions.h
+++ util/helper_functions.h
@@ -1,8 +1,10 @@
 #ifndef HELPER_FUNCTIONS_H
 #define HELPER_FUNCTIONS_H
 #include <random>
+#include <chrono>
 namespace HelperFunctions {
   //devuelve un entero random entre a y b
+  //PRE: b debe ser mayor que a
   int random_int(int a, int b);
   double distance(double x1, double x2, double y1, double y2);
   unsigned int difference(int x, int y);
