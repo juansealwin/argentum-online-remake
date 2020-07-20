6c69702f1e35f9ce7ec183b51707e09c8906164c
rodrigosettle <rodrigo@settlenetwork.com>
2020-07-20 03:52:58 -0300

    msgpack para guardar map en archivo

--- server/files_handler.cpp
+++ server/files_handler.cpp
@@ -2,6 +2,8 @@
 
 #include <fstream>
 #include <iostream>
+#include <msgpack.hpp>
+#include <unordered_map>
 
 #include "serializer.h"
 
@@ -10,6 +12,40 @@
 FilesHandler::FilesHandler() : mutex() {}
 
 FilesHandler::~FilesHandler() {}
+
+void hola() {
+  std::unordered_map<std::string, int> map = {
+      {"Rodrigo", 1}, {"sdssds", 21212}, {"FEfplepelfp", 848}};
+  std::stringstream buffer;
+  msgpack::pack(buffer, map);
+  std::ofstream players_file_position(
+      "../../server/status/players_file_position", std::ios::binary);
+  players_file_position << buffer.rdbuf();
+  players_file_position.close();
+}
+
+void chau() {
+  std::vector<unsigned char> buffer;
+  std::ifstream players_file_position(
+      "../../server/status/players_file_position", std::ios::binary);
+  players_file_position.seekg(0, std::ios_base::end);
+  std::streampos file_size = players_file_position.tellg();
+  std::cout << file_size << std::endl;
+  buffer.resize(file_size);
+  players_file_position.seekg(0, std::ios_base::beg);
+  players_file_position.read((char*)&buffer[0], file_size);
+  const std::string buffer_str(buffer.begin(), buffer.end());
+  const char* buffer_chars = buffer_str.c_str();
+  msgpack::object_handle oh =
+      msgpack::unpack(buffer_chars, buffer_str.size());
+  msgpack::object deserialized = oh.get();
+  std::unordered_map<std::string, int> map;
+  deserialized.convert(map);
+  std::unordered_map<std::string, int> expected_map = {
+      {"Rodrigo", 1}, {"sdssds", 21212}, {"fEfplepelfp", 848}};
+  const bool equal = expected_map == map;
+  std::cout << "son iguales? " << equal << std::endl;
+}
 
 void FilesHandler::save_player_status(Hero* hero) {
   std::unique_lock<std::mutex> lock(mutex);
@@ -37,6 +73,8 @@ Hero* FilesHandler::get_player_status(const std::string player_name,
                                       Json::Value& entities_cfg, int id, int x,
                                       int y, Map* map) {
   std::unique_lock<std::mutex> lock(mutex);
+  hola();
+  chau();
   return nullptr;
 
   std::vector<unsigned char> player_serialization;
